c3077c5131524e07d054a1f89f6b13856e3ca67d
Kevin Bui <kbuicuong@gmail.com>
2018-12-28 11:56:13 -0500

    counting duplicates

--- codewars.js
+++ codewars.js
@@ -11,3 +11,22 @@ function filter_list(l) {
   }
   return newArray;
 }
+
+//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits
+//that occur more than once in the input string. The input string can be assumed to contain only alphabets 
+//(both uppercase and lowercase) and numeric digits.
+
+function duplicateCount(text){
+  var lower = text.toLowerCase();
+  var count = 0;
+  var used = [];
+  
+  lower.split('').forEach(function(letter) {
+    if (!used.includes(letter) && (lower.split(letter).length - 1) > 1) {
+      count++;
+      used.push(letter);
+    }
+  });
+  
+  return count;
+}
