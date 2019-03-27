4ce40a355acc02f3cc64e432bb80f0c33a9629bc
Kevin Bui <kbuicuong@gmail.com>
2019-03-27 19:27:20 -0400

    day 22

--- JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -3,3 +3,17 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+
+    //returns a promise so then...
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false} )
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream); //put it in the browser
+            video.play();
+        })
+}
+
+getVideo();
